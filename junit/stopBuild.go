110ebbd4ea06830f47ab7fb3bd4e1a57caade3a0
Siva K <sivakwalker3@gmail.com>
2024-05-17 09:30:53 +0530

    feat: adding checks to validate testsuites before xml generation

--- junit/stopBuild.go
+++ junit/stopBuild.go
@@ -2,21 +2,27 @@ package junit
 
 func (jr *JUnitReporter) StopBuild() (string, error) {
 
-	err := generateXMLFromTestSuites(jr.buildDetails.buildIdentifier, jr.testSuites)
-	if err != nil {
-		return "", err
-	}
+	if len(jr.testSuites) > 0 {
+		err := generateXMLFromTestSuites(jr.buildDetails.buildIdentifier, jr.testSuites)
+		if err != nil {
+			return "", err
+		}
+		createZipFolderForUploader(jr.buildDetails.buildIdentifier)
 
-	createZipFolderForUploader(jr.buildDetails.buildIdentifier)
+		// Send this xml file to o11y api
+		respMessage, uploaderError := O11yJunitUploader(jr.buildDetails)
+		if uploaderError != nil {
+			return "", uploaderError
+		}
 
-	// Send this xml file to o11y api
-	respMessage, uploaderError := O11yJunitUploader(jr.buildDetails)
-	if uploaderError != nil {
-		return "", uploaderError
+		// Delete the created file & reset the inmemory to empty(default) values
+		jr.resetData()
+
+		return respMessage, nil
 	}
 
 	// Delete the created file & reset the inmemory to empty(default) values
 	jr.resetData()
 
-	return respMessage, nil
+	return "Build stop is successful", nil
 }
